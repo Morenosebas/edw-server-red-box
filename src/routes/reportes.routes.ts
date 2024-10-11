d48f1955fd1aa62b8ed623fbcdcab47e2ba02df3
johang31m <johangonzalez282971@correo.itm.edu.co>
2024-10-11 00:27:56 -0500

    fec

--- src/routes/reportes.routes.ts
+++ src/routes/reportes.routes.ts
@@ -146,7 +146,17 @@ router.patch(
       }
 
       // Extraer los campos que se pueden actualizar del cuerpo de la solicitud
-      const { nota, name_tecnico, field: fieldChange = "", fecha } = req.body;
+      const {
+        nota,
+        name_tecnico,
+        field: fieldChange = "",
+        fecha,
+      }: {
+        nota: string;
+        name_tecnico: string;
+        field: string;
+        fecha: string;
+      } = req.body;
 
       // Crear un objeto para almacenar las actualizaciones
       const actualizaciones: Partial<{
